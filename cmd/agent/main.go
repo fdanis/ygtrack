6d5a394a5c878ace708f524d8987453d5ee65917
danis <danis.nsu@gmail.com>
2022-12-16 15:18:22 +0300

    increment 7

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -17,7 +17,7 @@ import (
 )
 
 type Conf struct {
-	Address        string        `env:"ADDRESS" envDefault:"localhost:8080"`
+	Address        string        `env:"ADDRESS"`
 	PollInterval   time.Duration `env:"POLL_INTERVAL"`
 	ReportInterval time.Duration `env:"REPORT_INTERVAL"`
 }
@@ -30,7 +30,7 @@ func readEnv(config *Conf) {
 }
 
 func readFlags(config *Conf) {
-	flag.StringVar(&config.Address, "a", ":8080", "host for server")
+	flag.StringVar(&config.Address, "a", "localhost:8080", "host for server")
 	flag.DurationVar(&config.PollInterval, "p", time.Second*2, "interval fo pooling metrics")
 	flag.DurationVar(&config.ReportInterval, "r", time.Second*10, "interval fo report")
 }
